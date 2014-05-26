e688a1f07708976dfa74012f01b08a81e57cdb08
tomas garcia <toemossgarcia@gmail.com>
2014-05-26 17:30:15 -0500

    fixed delete

--- preprocessing/clean_up_nones.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sed 's/None/NA/g' results/out1.csv > results/revised1.csv
-sed 's/None/NA/g' results/out2.csv > results/revised2.csv
-sed 's/None/NA/g' results/out3.csv > results/revised3.csv
-sed 's/None/NA/g' results/out4.csv > results/revised4.csv
-
